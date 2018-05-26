515cba912301b445902b51c864655a580f9376df
Kyle Pfromer <kpfromer2@gmail.com>
2018-05-25 22:42:52 -0600

    Update App to use Footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
 import PropTypes from "prop-types";
 
+import Footer from "./Footer/Footer";
+
 class App extends Component {
+
+  static propTypes = {
+    socialMedia: PropTypes.arrayOf(PropTypes.shape({
+      url: PropTypes.string.isRequired,
+      icon: PropTypes.object.isRequired
+    })).isRequired
+  };
 
   render() {
     return (
       <div>
+        <Footer socialMedia={this.props.socialMedia}/>
       </div>
     );
   }
