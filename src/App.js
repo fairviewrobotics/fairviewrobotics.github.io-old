a1a957a2aedbcde74bf9812e5af73acefa494e67
Kyle Pfromer <kpfromer2@gmail.com>
2018-05-25 22:39:59 -0600

    Add basic react app components

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import PropTypes from "prop-types";
+
+class App extends Component {
+
+  render() {
+    return (
+      <div>
+      </div>
+    );
+  }
+}
+
+export default App;
