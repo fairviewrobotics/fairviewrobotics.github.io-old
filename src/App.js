5913dbd5184342403c8010552ae4846679003deb
Kyle Pfromer <kpfromer2@gmail.com>
2018-05-26 02:13:26 -0600

    Add basic routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import PropTypes from "prop-types";
+import { BrowserRouter, Route } from "react-router-dom";
 
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
+import RouteChange from "./RouteChange";
 
 class App extends Component {
 
@@ -13,12 +15,37 @@ class App extends Component {
     })).isRequired
   };
 
+  state = {
+    isMainPage: true
+  };
+
+  handleRouteChange = ({ pathname }) => {
+    if (this.state.isMainPage && (
+      pathname === '/about' ||
+      pathname === '/sponsors' ||
+      pathname === '/gallery' ||
+      pathname === '/calendar' )) {
+      this.setState({ isMainPage: false });
+    } else if (!this.state.isMainPage && pathname === '/') {
+      this.setState({ isMainPage: true });
+    }
+  };
+
   render() {
     return (
-      <div>
-        <Header isCollapsed={false} />
-        <Footer fixed={true} socialMedia={this.props.socialMedia}/>
-      </div>
+      <BrowserRouter>
+        <div>
+          <RouteChange onRouteChange={this.handleRouteChange}/>
+
+          <Header isCollapsed={!this.state.isMainPage}/>
+          <Route exact path="/" render={() => <h1>Home</h1>}/>
+          <Route exact path="/about" render={() => <h1>About</h1>}/>
+          <Route exact path="/sponsors" render={() => <h1>sponsors</h1>}/>
+          <Route exact path="/gallery" render={() => <h1>gallery</h1>}/>
+          <Route exact path="/calendar" render={() => <h1>Calendar</h1>}/>
+          <Footer fixed={this.state.isMainPage} socialMedia={this.props.socialMedia}/>
+        </div>
+      </BrowserRouter>
     );
   }
 }
