0f699c206dd224d873452d7bbfe996981bb39464
Kyle Pfromer <kpfromer2@gmail.com>
2018-05-26 02:24:06 -0600

    Add NotFound component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import PropTypes from "prop-types";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
+import RouteChange from "./RouteChange";
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
-import RouteChange from "./RouteChange";
+import NotFound from "./NotFound/NotFound";
 
 class App extends Component {
 
@@ -38,11 +39,17 @@ class App extends Component {
           <RouteChange onRouteChange={this.handleRouteChange}/>
 
           <Header isCollapsed={!this.state.isMainPage}/>
-          <Route exact path="/" render={() => <h1>Home</h1>}/>
-          <Route exact path="/about" render={() => <h1>About</h1>}/>
-          <Route exact path="/sponsors" render={() => <h1>sponsors</h1>}/>
-          <Route exact path="/gallery" render={() => <h1>gallery</h1>}/>
-          <Route exact path="/calendar" render={() => <h1>Calendar</h1>}/>
+
+          <Switch>
+            <Route exact path="/" render={() => <h1>Home</h1>}/>
+            <Route exact path="/about" render={() => <h1>About</h1>}/>
+            <Route exact path="/sponsors" render={() => <h1>sponsors</h1>}/>
+            <Route exact path="/gallery" render={() => <h1>gallery</h1>}/>
+            <Route exact path="/calendar" render={() => <h1>Calendar</h1>}/>
+
+            <Route component={NotFound} />
+          </Switch>
+
           <Footer fixed={this.state.isMainPage} socialMedia={this.props.socialMedia}/>
         </div>
       </BrowserRouter>
---
+++ src/NotFound/NotFound.js
@@ -0,0 +1,13 @@
+import React, { PureComponent } from 'react';
+
+import './NotFound.css';
+
+export default class NotFound extends PureComponent {
+  render() {
+    return (
+      <div>
+        <h1 className="title">Page Not Found</h1>
+      </div>
+    );
+  }
+}
